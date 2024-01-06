9a9726c806e949c4a3cd43cd4934c5f757e55a4e
Ljupche Vasilev <ljupche.vasilev@gmail.com>
2024-01-06 15:28:14 +0100

    Chain Resolvers

--- src/Application/resolvers/index.ts
+++ src/Application/resolvers/index.ts
@@ -1,6 +1,37 @@
 import { City, Launch, Launchpad, Rocket } from "../../Domain/types";
 
 export const resolvers = {
+  Launch: {
+    rocket: async (
+      { rocket }: { rocket: string },
+    ) => {
+      const rocketData: Rocket = await fetch(
+        `https://api.spacexdata.com/v4/rockets/${rocket}`
+      ).then((res) => res.json()) as Rocket;
+
+      return rocketData;
+    },
+    launchpad: async (
+      { launchpad }: { launchpad: string },
+    ) => {
+      const launchpadData: Launchpad = await fetch(
+        `https://api.spacexdata.com/v4/launchpads/${launchpad}`
+      ).then((res) => res.json()) as Launchpad;
+
+      return launchpadData;
+    },
+  },
+  Launchpad: {
+    city: async (
+      { latitude, longitude }: { latitude: number, longitude: number },
+    ) => {
+      const city: City = await fetch(
+        `https://api.geodatasource.com/city?key=50QWVCVPE7ID8PGZNTUPYTKBPANQ5BTH&format=json&lat=${latitude}&lng=${longitude}`
+      ).then((res) => res.json()) as City;
+
+      return city;
+    },
+  },
   Query: {
     launch: async (
       parent: any,
@@ -9,22 +40,6 @@ export const resolvers = {
       const launch: Launch = await fetch(`https://api.spacexdata.com/v4/launches/${id}`).then(
         (res: Response) => res.json()
       ) as Launch;
-
-      const launchpad = await fetch(
-        `https://api.spacexdata.com/v4/launchpads/${launch.launchpad}`
-      ).then((res) => res.json()) as Launchpad;
-
-      const city = await fetch(
-        `https://api.geodatasource.com/city?key=50QWVCVPE7ID8PGZNTUPYTKBPANQ5BTH&format=json&lat=${launchpad.latitude}&lng=${launchpad.longitude}`
-      ).then((res) => res.json()) as City;
-
-      const rocket = await fetch(
-        `https://api.spacexdata.com/v4/rockets/${launch.rocket}`
-      ).then((res) => res.json()) as Rocket;
-
-      launchpad.city = city;
-      launch.launchpad = launchpad;
-      launch.rocket = rocket;
 
       return launch;
     },
